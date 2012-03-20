ea8245221fb07e27ca15c2c843039ae8422037c1
jslim <jslim@ljs-PC.inca.local>
2012-03-20 13:20:05 +0900

    changed splash thread to asynctask

--- src/com/bridginggoodbiz/DB/LoginJSON.java
+++ src/com/bridginggoodbiz/DB/LoginJSON.java
@@ -15,33 +15,27 @@ public class LoginJSON {
 	private static final String PARAM_RESULT_CODE = "resultCode";
 	private static final String PARAM_RESULT_MSG = "resultMsg";
 
-	public static boolean doLogin(final String username, final String password){
-		try {
-			String targetURL = CONST.API_LOGIN_URL;
-			String requestParam = "";
+	public static boolean doLogin(final String username, final String password) throws Exception{
+		String targetURL = CONST.API_LOGIN_URL;
+		String requestParam = "";
 
-			String[][] param = { 
-					{PARAM_USERNAME, CRYPTO.decrypt(CONST.CRYPT_SEED, username)}, 
-					{PARAM_PASSWORD, CRYPTO.decrypt(CONST.CRYPT_SEED, password)}
-			};
-			requestParam = BgHttpHelper.generateParamData(param);
+		String[][] param = { 
+				{PARAM_USERNAME, CRYPTO.decrypt(CONST.CRYPT_SEED, username)}, 
+				{PARAM_PASSWORD, CRYPTO.decrypt(CONST.CRYPT_SEED, password)}
+		};
+		requestParam = BgHttpHelper.generateParamData(param);
 
-			String jsonStr = BgHttpHelper.requestHttpRequest(targetURL, requestParam, "POST");
+		String jsonStr = BgHttpHelper.requestHttpRequest(targetURL, requestParam, "POST");
 
-			JSONObject jsonObject = new JSONObject(jsonStr);
+		JSONObject jsonObject = new JSONObject(jsonStr);
 
-			if(jsonObject.getString(PARAM_RESULT_CODE).charAt(0) == 'S'){
-				updateBusienssWithJSON(jsonObject);
-				return true;
-			} else {
-				Log.d("BgBizDB", "makeDonation failed: "+jsonObject.getString(PARAM_RESULT_MSG));
-				return false;
-			}
-		} catch (Exception e){
-			// Handle all exception
-			Log.d("BgDB", "Exception occured: "+e.getLocalizedMessage());
+		if(jsonObject.getString(PARAM_RESULT_CODE).charAt(0) == 'S'){
+			updateBusienssWithJSON(jsonObject);
+			return true;
+		} else {
+			Log.d("BgBizDB", "makeDonation failed: "+jsonObject.getString(PARAM_RESULT_MSG));
+			return false;
 		}
-		return false;
 	}
 
 	private static void updateBusienssWithJSON(JSONObject jsonObj) throws Exception{
--- src/com/bridginggoodbiz/SplashController.java
+++ src/com/bridginggoodbiz/SplashController.java
@@ -1,24 +1,24 @@
 package com.bridginggoodbiz;
 
-import com.bridginggoodbiz.DB.LoginJSON;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.bridginggoodbiz.DB.LoginJSON;
+
 public class SplashController extends Activity{
-	private ProgressDialog mProgressDialog;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash_layout);
-		
+
 		ConnectivityManager manager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo ni = manager.getActiveNetworkInfo();
 		if(ni== null || !ni.isConnected())
@@ -26,58 +26,74 @@ public class SplashController extends Activity{
 			Toast.makeText(getApplicationContext(), "No network connection found. Please check your network status.", Toast.LENGTH_LONG).show();
 		} 
 
-		mProgressDialog = ProgressDialog.show(this, "", "Loading... Please wait", true, false);
-		Business.init();
-		
-		Thread splashTread = new Thread() {
-			@Override
-			public void run() {
-				long startTime = android.os.SystemClock.uptimeMillis();
+		SplashLoadingAsyncTask splashLoadingAsyncTask = new SplashLoadingAsyncTask(SplashController.this);
+		splashLoadingAsyncTask.execute();
+	}
 
-				/*
-				 *===============START Loading Job==================== 
-				 */
+	//SplashLoadingAsyncTask
+	private class SplashLoadingAsyncTask extends AsyncTask<Context, Boolean, Boolean>
+	{
+		private ProgressDialog mProgressDialog;
+		private Context mContext;
+		private long mStartTime;
+		private String mErrStr;
+
+		public SplashLoadingAsyncTask(Context context){
+			this.mContext = context;
+		}
+
+		//Display progress dialog
+		protected void onPreExecute()
+		{
+			mProgressDialog = ProgressDialog.show(this.mContext, "", "Loading... Please wait", true, false);
+			Business.init();
+		}
+
+		//Do login
+		protected Boolean doInBackground(Context... contexts)
+		{
+			try{
+				mStartTime = android.os.SystemClock.uptimeMillis();
+
+				//Load stored data from the device
 				BusinessStore.loadUserSession(getApplicationContext());
 				
 				//Check if saved user token exists or not
-				boolean isLoginSuccess = isBusinessAutoLoginSuccessful();
+				boolean isLoginSuccess = LoginJSON.doLogin(Business.getBizName(), Business.getBizPassword());
 				Log.d("BGB", "isBusinessAutoLogin:  "+isLoginSuccess);
-
-				// Decide where to redirect user depending on isLoginSuccess
-				Class<?> targetClass = isLoginSuccess? MainActivity.class:LoginActivity.class;
-
-				long durationTime = android.os.SystemClock.uptimeMillis() - startTime;
-				Log.d("BGB", "Login time taken: "+durationTime);
-				if (durationTime <=CONST.SPLASH_DELAY) {
-					try {
-						synchronized(this){
-							//Pause the application for remaining time to meet SPLASH_DELAY
-							wait(CONST.SPLASH_DELAY-durationTime);
-						}
-					} catch (Exception e) {
-						Log.d("BG", "spalashThread Exception: "+e.getLocalizedMessage());
-					}
-				}
-
-				/*
-				 *===============END Loading Job==================== 
-				 */
-
-				mProgressDialog.dismiss();
-				finish();
-				startActivity(new Intent().setClass(SplashController.this, targetClass));
+			} catch(Exception e) {
+				mErrStr = e.getLocalizedMessage();
 			}
-		};
-		splashTread.start();
-	}
+			return false;
+		}
+		protected void onPostExecute(final Boolean isLoginSuccess)
+		{
+			if(mProgressDialog.isShowing())
+			{
+				mProgressDialog.dismiss();
+			}
 
-	/**
-	 * Returns whether login attempt using stored credentials was successful or not.
-	 * @return True if login is success
-	 */
-	private boolean isBusinessAutoLoginSuccessful(){
-		LoginJSON.doLogin(Business.getBizName(), Business.getBizPassword());
-		return false;
-	}
+			// Decide where to redirect user depending on isLoginSuccess
+			Class<?> targetClass = isLoginSuccess? MainActivity.class:LoginActivity.class;
 
+			long durationTime = android.os.SystemClock.uptimeMillis() - mStartTime;
+			Log.d("BGB", "Login time taken: "+durationTime);
+			if (durationTime <=CONST.SPLASH_DELAY) {
+				try {
+					//Pause the application for remaining time to meet SPLASH_DELAY
+					wait(CONST.SPLASH_DELAY-durationTime);
+				} catch (Exception e) {
+					Log.d("BG", "spalashThread Exception: "+e.getLocalizedMessage());
+					mErrStr = e.getLocalizedMessage();
+				}
+			}
+			mProgressDialog.dismiss();
+
+			if(!isLoginSuccess && mErrStr == null)
+				Toast.makeText(mContext, "Error occured:"+mErrStr, Toast.LENGTH_SHORT).show();
+
+			finish();
+			startActivity(new Intent().setClass(SplashController.this, targetClass));
+		}
+	}
 }
